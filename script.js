db107d04262751912ca725750e6c702c632fc414
Grapensparr <113124538+Grapensparr@users.noreply.github.com>
2022-11-27 17:20:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -91,9 +91,9 @@ function logIn() {
         loggedOutView.style.display = "none"
         loggedInView.style.display = "block"
         header.innerHTML = "Welcome to Santa's Workshop " + username.value + "!"
-	welcomeUsername.innerHTML += "Merry Christmas " + username.value + "!"
-
-        localStorage.setItem("loggedInUser", username.value)
+	      
+	localStorage.setItem("loggedInUser", username.value)
+	welcomeUsername.innerHTML += "Merry Christmas " + localStorage.getItem("loggedInUser") + "!"
 
         //Keep user logged in
         localStorage.setItem("isLoggedIn", true)
